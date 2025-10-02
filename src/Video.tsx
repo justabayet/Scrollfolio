cd4756b262f6a3985f0ba26ffd4823e87e5380bf
justabayet <anthony.bayet@hotmail.com>
2025-10-02 14:53:40 +0900

    Add loading indicator

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,27 +1,60 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 interface VideoProps {
     src: string
     blur?: number
 }
 
+const keyframes = `
+@keyframes spin {
+    0% { transform:  translate(-50%, -50%) rotate(0deg);}
+    100% { transform:  translate(-50%, -50%) rotate(360deg);}
+}
+`;
+
 function Video({ src, blur = 15 }: VideoProps) {
     const frontVideoRef = useRef<HTMLVideoElement>(null)
     let posterSrc = src.slice(0, -3) + 'webp'
     posterSrc = posterSrc.replace("/videos/", "/thumbnails/")
+
+    const [isVideoLoaded, setIsVideoLoaded] = useState(false)
+
+    const handleLoadedData = () => {
+        setIsVideoLoaded(true)
+        frontVideoRef.current?.play()
+    }
 
     return (
         <div style={{
             position: 'relative', height: '100%', width: '100%',
             overflow: 'hidden'
         }}>
+            <style>{keyframes}</style>
+            {!isVideoLoaded &&
+                <div style={{
+                    width: '50px',
+                    height: '50px',
+                    position: 'absolute',
+                    zIndex: 11,
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    boxSizing: 'border-box',
+                    borderRadius: '50%',
+                    animation: '1.2s cubic-bezier(0.35, 0.01, 0.67, 0.98) 0s infinite normal none running spin',
+                    borderColor: 'rgb(153, 153, 153) rgb(236, 236, 236) rgb(236, 236, 236)',
+                    borderWidth: '2px',
+                    borderStyle: 'solid',
+                }}>
+                </div>
+            }
             {/* Front Video */}
             <video
                 ref={frontVideoRef}
-                autoPlay
                 muted
                 loop
                 playsInline
+                onLoadedData={handleLoadedData}
                 poster={posterSrc}
                 style={{
                     height: '100%',
