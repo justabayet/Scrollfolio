229522eee0f7676a6ca484ca153aed68418e8091
justabayet <anthony.bayet@hotmail.com>
2026-03-09 18:33:20 +0100

    Fix text

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -20,31 +20,38 @@ function HomePage({ nextElement }: HomePageProps) {
                 width: '100%',
                 height: '100%',
                 // background: 'linear-gradient( #0f293eff, #141414ff )',
-                backgroundColor: "#141414ff",
+                // backgroundColor: "#141414ff",
+                backgroundColor: 'var(--color-primary)',
                 color: 'var(--color-accent)',
+                // color: 'white',
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',
                 justifyContent: 'center',
-                fontFamily: 'Lexend Deca'
+                fontFamily: 'Lexend Deca',
             }}>
                 <div style={{
                     maxWidth: '500px',
                     padding: '12px'
                 }}>
                     <h1>Anthony Bayet</h1>
-                    <h2>Creative Developer</h2>
-                    <p>Belgium 🇧🇪 {EMAIL_ADDRESS}</p>
+                    <h2 >Creative Developer</h2>
+                    <p style={{
+                        fontFamily: 'system-ui',
+                        fontWeight: 200
+                    }}>Belgium 🇧🇪 {EMAIL_ADDRESS}</p>
                     <p style={{
                         padding: '24px 16px 0px 16px',
-                        textAlign: 'left'
+                        textAlign: 'left',
+                        fontFamily: 'system-ui',
+                        fontWeight: 200
                     }}>
-                        <span>Crafting</span> websites to bring life to <b>your creative</b> vision.
+                        I build web experiences to bring life to your creative vision.
                         <br /><br />
-                        Working through the <b>entire web stack</b>, using <b>React, Three.js, GLSL and Node.js</b> to build fully working web experiences that are <b>smooth, innovative and engaging</b>.
+                        {'<'}React{'>'} {'<'}Three.js{'>'} {'<'}WebGL{'>'} {'<'}Node.js{'>'}
                     </p>
                 </div>
-                <div style={{ height: '30%' }}></div>
+                <div style={{ height: '20%' }}></div>
             </div>
 
             <div
